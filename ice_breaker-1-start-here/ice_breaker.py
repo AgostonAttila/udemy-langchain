44c29e857167c8493c663ac656f826ec3291db79
Atesz014 <atesz014@gmail.com>
2024-03-16 20:14:13 +0100

    add tools project

--- ice_breaker-1-start-here/ice_breaker.py
+++ ice_breaker-1-start-here/ice_breaker.py
@@ -1,19 +1,16 @@
 """from dotenv import load_dotenv"""
+
 from langchain.chains import LLMChain
 from langchain_core.prompts import PromptTemplate
 from langchain_community.chat_models import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
-
+from third_parties.linkedin import scrape_linkedin_profile
 
 
 if __name__ == "__main__":
     """load_dotenv()"""
 
     print("Hello, Langchain!")
-
-    information = """ 
-    Steven Paul Jobs (February 24, 1955 – October 5, 2011) was an American businessman, inventor, and investor best known for co-founding the technology giant Apple Inc. Jobs was also the founder of NeXT and chairman and majority shareholder of Pixar. He was a pioneer of the personal computer revolution of the 1970s and 1980s, along with his early business partner and fellow Apple co-founder Steve Wozniak.
-    """
 
     summary_template = """
     given the Linkedin information {information} about a person from I want you to create:
@@ -22,13 +19,13 @@ if __name__ == "__main__":
     """
 
     summary_prompt_template = PromptTemplate(
-    input_variables=["information"], template=summary_template
-)
+        input_variables=["information"], template=summary_template
+    )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
-    res = chain.invoke(input={"information": information})
-  
 
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
 
+    print(chain.run(input={"information": information}))
